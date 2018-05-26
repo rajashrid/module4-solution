dd38633cf16ef94ec791a31f1a2715029e460905
rajashrid <rajashrideshmukh01@gmail.com>
2018-05-26 17:42:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+( function (){
+       var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+       for (name in names) {
+          var firstLetter = names[name].charAt(0);
+          var lowerCase = firstLetter.toLowerCase();
+
+         if (lowerCase == 'j') {
+            byeSpeaker.speak(names[name]);
+         } else {
+            helloSpeaker.speak(names[name]);
+         }
+       }
+})();
